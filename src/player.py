4ac7d9bd1038dad9435a41dcbb9bd6abe420e5c9
orpsisdope <you@example.com>
2025-12-02 15:21:09 +0100

    Created_two_functions_for_addingscores_and_resettingscores

--- src/player.py
+++ src/player.py
@@ -2,3 +2,9 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.score = 0
+
+    def add_score(self, point):
+        pass
+
+    def eset_score(self):
+        pass
