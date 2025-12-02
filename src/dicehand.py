cd103a4d0fe392e89ace3f34a26eac117bb6b3bf
orpsisdope <you@example.com>
2025-12-02 15:15:20 +0100

    i_have_added_function_for_all_dice_rolling

--- src/dicehand.py
+++ src/dicehand.py
@@ -3,3 +3,6 @@ from src.dice import Dice
 class DiceHand:
     def __init__(self, dice_num = 1):
         self.dice = [Dice() for _ in range (dice_num)]
+
+    def roll_all(self):
+        return [i.roll() for i in self.dice]
