95e32f78d6adb660981e0cb604960fd5f5169897
ahmeddoner <altemimiahmed779@gmail.com>
2025-12-05 00:59:52 +0100

    Update test_dicehand.py

--- tests/test_dicehand.py
+++ tests/test_dicehand.py
@@ -7,3 +7,9 @@ def test_roll_all_returns_list():
     hand = DiceHand(1)
     result = hand.roll_all()
     assert isinstance(result, list)
+
+
+def test_roll_all_contains_integers():
+    hand = DiceHand(3)
+    rolls = hand.roll_all()
+    assert all(isinstance(r, int) for r in rolls)
