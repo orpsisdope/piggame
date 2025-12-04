96bf40a83c767505ba229e6148873ca71d98f0b4
orpsisdope <orpsisdopeaf@gmail.com>
2025-12-04 22:38:53 +0100

    Iam_selecting_the_values_at_where_it_should_stop

--- src/intelligence.py
+++ src/intelligence.py
@@ -4,3 +4,10 @@ class Intelligence:
     def __init__(self, level="easy"):
         self.level = level
         print(f"you will be playing in {self.level} mode")
+
+    def should_hold(self, turn_score):
+        match self.level:
+            case "easy":
+                return turn_score >=  12
+            case "medium":
+                return turn_score >=  15
