8324eed3bf52a53f310b3e0f048c661242d5f869
ahmeddoner <altemimiahmed779@gmail.com>
2025-12-05 00:51:21 +0100

    Update test_dice.py

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -6,6 +6,7 @@ from src.dice import Dice
 def test_roll_returns_integer():
     dice = Dice()
     assert isinstance(dice.roll(), int)    assert isinstance(dice.roll(), int)
+    assert isinstance(dice.roll(), int)
 
 def test_roll_in_range():
     dice = Dice()
