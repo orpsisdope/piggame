d626485b3048e4e02699ba8892221d30e48988aa
laserchapo <nomialhadad@gmail>
2025-12-05 00:33:33 +0100

    Update highscore.py

--- src/highscore.py
+++ src/highscore.py
@@ -16,3 +16,10 @@ class HighScore:
     def __init__(self, filename="highscore.json"):
         self.filename = filename
         if not os.path.exists(filename):
+              with open(filename, "w", encoding="utf-8") as file:
+                json.dump({}, file)
+
+
+
+
+    def _load(self):
