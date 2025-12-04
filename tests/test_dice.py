fff0503301065ea3c74e1bb38c396a9a1421d646
ahmeddoner <altemimiahmed779@gmail.com>
2025-12-05 00:56:10 +0100

    Update test_dice.py

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -18,3 +18,8 @@ def test_roll_distribution():
     rolls = [dice.roll() for _ in range(50)]
     assert all(1 <= r <= 6 for r in rolls)
     assert len(set(rolls)) > 1
+
+
+def test_multiple_instances():
+    d1, d2 = Dice(), Dice()
+    assert d1.sides == 6 and d2.sides == 6
