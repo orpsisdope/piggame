6893518824c93ef49e84f1959b785bba8c62127d
ahmeddoner <altemimiahmed779@gmail.com>
2025-12-05 01:00:22 +0100

    Update test_dicehand.py

--- tests/test_dicehand.py
+++ tests/test_dicehand.py
@@ -13,3 +13,9 @@ def test_roll_all_contains_integers():
     hand = DiceHand(3)
     rolls = hand.roll_all()
     assert all(isinstance(r, int) for r in rolls)
+
+def test_roll_range():
+    hand = DiceHand(2)
+    rolls = hand.roll_all()
+    for r in rolls:
+        assert 1 <= r <= 6
