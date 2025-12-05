cb9d8267f7010e0d0ec40cd20636a3084defc1ea
orpsisdope <orpsisdopeaf@gmail.com>
2025-12-05 14:03:32 +0100

    I_forgot_to_add_game_or_i_thought_i_did

---
+++ src/game.py
@@ -0,0 +1,18 @@
+class Game:
+
+    def __init__(
+        self,
+        player1_name,
+        player2_name,
+        ai_mode=False,
+        ai_level="easy",
+        dice_count=1,                        
+        target_score=100
+    ):
+        self.players = [Player(player1_name), Player(player2_name)]
+        self.dicehand = DiceHand(dice_count)  
+        self.highscore = HighScore()
+        self.current = 0
+        self.ai_mode = ai_mode
+        self.intelligence = Intelligence(ai_level)
+        self.target_score = target_score
